400e8ec54746c167aaa0c26f20ff4a6f3654cb64
Blake <blake.enyart@gmail.com>
2019-04-30 13:50:08 -0600

    WIP typing time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".element").typed({
-        strings: ["^300Hi there!^1000 I'm Blake.^500", "Let's start to make some change.^2000"],
+        strings: ["^1000Hi there!^1000 I'm Blake.^500", "Let's start to make some change.^2000"],
         typeSpeed: 0
     });
 });
