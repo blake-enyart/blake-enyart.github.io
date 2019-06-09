d5704fbeb8eb1866855da9e55ca66a9d901a9228
Blake <blake.enyart@gmail.com>
2019-06-09 16:52:44 -0600

    Edit text to fit iPhone screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".element").typed({
-        strings: ["^1000Hi there!^1000 I'm Blake.^500", "I'm a computer programmer.^1500", "Let's start to make some change.^2000"],
+        strings: ["^1000Hi there!^1000 I'm Blake.^500", "I'm a computer programmer.^1500", "Let's make some change.^2000"],
         typeSpeed: 0
     });
 });
