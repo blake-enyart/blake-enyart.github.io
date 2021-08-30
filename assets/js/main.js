14fb7b97a38e98874c555bf4fbea4b00c01c261b
Blake Enyart <38588295+blake-enyart@users.noreply.github.com>
2021-08-29 19:03:06 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".element").typed({
-        strings: ["^1000Hi there!^1000 I'm Blake.^500", "I'm a computer programmer.^1500", "Let's make some change.^2000"],
+        strings: ["^1000Hi there!^1000 I'm Blake.^500", "I'm a biochemist turned data nerd^1500", "Reach out and let's chat.^2000"],
         typeSpeed: 0
     });
 });
